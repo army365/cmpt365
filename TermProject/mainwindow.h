d90e50c6e68d354804572d10feb5bbd127a5572d
kaypnma <kay.arellano254@gmail.com>
2019-04-01 14:48:59 -0700

    Updated header

--- TermProject/mainwindow.h
+++ TermProject/mainwindow.h
@@ -2,6 +2,26 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QLayout>
+#include <QScrollArea>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QImageReader>
+#include <QImageWriter>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QSpinBox>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 
 namespace Ui {
 class MainWindow;
